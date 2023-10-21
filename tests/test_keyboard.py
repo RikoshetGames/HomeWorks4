6cabab3ff967bb0f46e35553ddd6e680162efce2
RikoshetGames <ilyalew@mail.ru>
2023-10-21 21:34:59 +0300

    HomeWork 15.2 update

--- tests/test_keyboard.py
+++ tests/test_keyboard.py
@@ -7,13 +7,9 @@ from src.keyboard import Keyboard
 def test_class_keyboard():
     kb = Keyboard('Dark Project KD87A', 9600, 5)
     assert str(kb) == "Dark Project KD87A"
-
     assert str(kb.language) == "EN"
-
     kb.change_lang()
     assert str(kb.language) == "RU"
-
-    # Сделали EN -> RU -> EN
     kb.change_lang()
     assert str(kb.language) == "EN"
 
